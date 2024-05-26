f53b1bda62b5896a2d1daed906d1a65c68d4c335
Danh8822 <danhphan8822@gmail.com>
2024-05-26 14:38:39 +0700

    deploy

--- apiConfig.js
+++ apiConfig.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// Tạo một instance cho server đầu tiên
-const backend1 = axios.create({
-  baseURL: 'https://bendshop.onrender.com',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// // Tạo một instance cho server đầu tiên
+// const backend1 = axios.create({
+//   baseURL: 'https://bendshop.onrender.com',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
-// Tạo một instance cho server thứ hai (Flask)
-const backend2 = axios.create({
-  baseURL: 'https://chatbotndshop.onrender.com',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// // Tạo một instance cho server thứ hai (Flask)
+// const backend2 = axios.create({
+//   baseURL: 'https://chatbotndshop.onrender.com',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
-export { backend1, backend2 };
+// export { backend1, backend2 };
